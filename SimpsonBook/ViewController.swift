6e083f5367b0a13d5fe4fac3bf85571652a5f599
Kemal Aslan <kemal.aslan.note@gmail.com>
2022-12-05 19:51:48 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Kemal Aslan on 5.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
